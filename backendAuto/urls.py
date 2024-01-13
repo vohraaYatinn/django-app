620196387b65c20ebac40035ba1b2c6ad540dbb1
vohraaYatinn <96340665+vohraaYatinn@users.noreply.github.com>
2024-01-13 22:30:09 +0530

    change

--- backendAuto/urls.py
+++ backendAuto/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static('/pdfs/', document_root=settings.STATICFILES_DIRS[0] + '/pdfs/')
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
